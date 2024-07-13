8e810a9b20835c605ebcbfc9900eda9fab174c89
ECHOFL <fliqqtw@outlook.fr>
2024-07-13 11:17:18 +0200

    New Update

---
+++ components/Grid.tsx
@@ -0,0 +1,20 @@
+import { gridItems } from "@/data";
+import { BentoGrid, BentoGridItem } from "./ui/bento-grid";
+
+const Grid = () => {
+  return (
+    <section id="about">
+      <BentoGrid>
+        {gridItems.map((item, i) => (
+          <BentoGridItem
+            id={item.id}
+            key={item.id}
+            title={item.title}
+            description={item.description}
+          />
+        ))}
+      </BentoGrid>
+    </section>
+  );
+};
+export default Grid;
---
+++ data/index.ts
@@ -0,0 +1,4 @@
+export const gridItems = [
+  { title: "Title1", description: "Desc1", id: 1 },
+  { title: "Title2", description: "Desc2", id: 2 },
+];
